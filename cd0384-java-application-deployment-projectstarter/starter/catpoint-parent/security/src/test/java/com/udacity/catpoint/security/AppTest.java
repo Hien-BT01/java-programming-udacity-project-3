94db13b4c4eb7f0e77b35492b8115c404f6748a5
Bùi Thế Hiển <hienbui25g@gmail.com>
2024-10-03 00:02:06 +0700

    finish project

---
+++ cd0384-java-application-deployment-projectstarter/starter/catpoint-parent/security/src/test/java/com/udacity/catpoint/security/AppTest.java
@@ -0,0 +1,15 @@
+package com.udacity.catpoint.security;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
